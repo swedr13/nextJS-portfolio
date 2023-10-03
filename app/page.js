5fa196c65c3b3b7f2b0c8d739e141a144a37e9a2
swedr13 <junelleXIII@gmail.com>
2023-10-04 03:14:34 +0800

    update navbar
    use the component in Navbar

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import styles from './page.module.css';
-import CardProject from './components/cardProject';
+import CardProject from './components/cardProject'
+import Navbar from './components/navbar';
+
+
 
 const Home = () => {
   return (
@@ -13,80 +16,22 @@ const Home = () => {
   <meta name="description" content="" />
   <meta name="author" content="" />
   <title>Junelle Suaybaguio Portfolio</title>
-  {/* Bootstrap core CSS */}
+
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-  {/* Custom fonts for this template */}
-  <link
-    href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900"
-    rel="stylesheet"
-  />
-  <link
-    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
-    rel="stylesheet"
-  />
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet"/>
   <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
   <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet" />
-  <link
-    href="vendor/simple-line-icons/css/simple-line-icons.css"
-    rel="stylesheet"
-  />
-  {/* Custom styles for this template */}
+  <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet"/>
   <link href="css/resume.min.css" rel="stylesheet" />
-  <nav
-    className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top"
-    id="sideNav"
-  >
-    <a className="navbar-brand js-scroll-trigger" href="#page-top">
-      <span className="d-block d-lg-none">Suaybaguio Portfolio</span>
-      <span className="d-none d-lg-block">
-        <img
-          className="img-fluid img-profile rounded-circle mx-auto mb-2"
-          src="img/profile.jpg"
-          alt=""
-        />
-      </span>
-    </a>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#about">
-            About
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#experience">
-            Experience
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#education">
-            Education
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#projects">
-            Projects
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#contact">
-            Contact
-          </a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+
+  <Navbar />
+
+
+
+
+
+
   <div className="container-fluid p-0">
     <section className="resume-section p-3 p-lg-5 d-flex d-column bg-light" id="about">
       <div className="my-auto">
